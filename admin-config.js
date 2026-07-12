bdaa9f810c4f7e49db64bfc14e7e387c80027ee3
karikaasdasdqw2e <m.karika2020@gmail.com>
2026-07-12 07:12:47 +0300

    Add isolated public config for admin dashboard

---
+++ admin-config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://cbwahudauwtpllabiqla.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_PvA9-yINBscGfnS_t127Jw_-wZo2ZTg",
+  VILLAGE_NAME: "إصطباري"
+};
